fef38716e565ee41d43198aa93c38408d0520b58
Jillian Werner <jillianlwerner@gmail.com>
2020-03-19 12:46:38 -0400

    Add basic functions for four main operators

---
+++ javascript.js
@@ -0,0 +1,26 @@
+function add(num1, num2) {
+    return num1 + num2;
+}
+
+function subtract(num1, num2) {
+    return num1 - num2;
+}
+
+function multiply(num1, num2) {
+    return num1 * num2;
+}
+
+function divide(num1, num2) {
+    return num1 / num2;
+}
+
+function operate(operator, num1, num2) {
+    const operators = {
+        "+": add,
+        "-": subtract,
+        "*": multiply,
+        "/": divide,
+    }
+
+    return operators[operator](num1, num2);
+}
