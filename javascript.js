9e3c840f58019b7611406740beda1ecf51429450
Jillian Werner <jillianlwerner@gmail.com>
2020-03-20 17:31:24 -0400

    Connect math functions to operator buttons and display result

--- javascript.js
+++ javascript.js
@@ -5,6 +5,7 @@ let numbersToCalculate = {
     workingOperator: "",
     secondNumber: "",
 };
+let lastButtonPressed = "";
 
 buttons.forEach(button => button.addEventListener('click', displayButton));
 
@@ -33,30 +34,54 @@ function displayButton(button) {
 
     let buttonID = button.target.id;
 
+    console.log(buttonID);
+    if (buttonID === "function-clear") {
+        numbersToCalculate["firstNumber"] = "";
+        numbersToCalculate["workingOperator"] = "";
+        numbersToCalculate["secondNumber"] = "";
+    }
+
+    console.log("Before anything is called:", numbersToCalculate["firstNumber"]);
+    console.log(numbersToCalculate["workingOperator"]);
+    console.log(numbersToCalculate["secondNumber"]);
+
+    if (buttonID === "operator-equals") {
+        numbersToCalculate["firstNumber"] = `${operate(numbersToCalculate["workingOperator"], numbersToCalculate["firstNumber"], numbersToCalculate["secondNumber"])}`;
+        numbersToCalculate["workingOperator"] = "";
+        numbersToCalculate["secondNumber"] = "";
+    }
+
     if (digitInput.hasOwnProperty(buttonID)) {
         if (buttonID === "digit-negative") {
             console.log("work in progress"); /* need to append negative to current number */
-        } else if (numbersToCalculate["workingOperator"] === "") {
+        } else if (numbersToCalculate["workingOperator"] === "" && lastButtonPressed !== "operator-equals") {
             numbersToCalculate["firstNumber"] += digitInput[buttonID]; /* if there is no operator, we're still adding digits to the first number */
+        } else if (numbersToCalculate["workingOperator"] === "" && lastButtonPressed === "operator-equals") {
+            numbersToCalculate["firstNumber"] = digitInput[buttonID];
         } else if (numbersToCalculate["workingOperator"] !== "") {
             numbersToCalculate["secondNumber"] += digitInput[buttonID]; /* if there IS an operator, add digits to the second number */
         };
     };
 
     if (operatorInput.hasOwnProperty(buttonID)) {
+        console.log("Inside operatorInput function:", numbersToCalculate["workingOperator"]);
         if (numbersToCalculate["workingOperator"] === "") {
             numbersToCalculate["workingOperator"] = operatorInput[buttonID];
         } else if (numbersToCalculate["workingOperator"] !== "") {
-            console.log("work in progress"); /* need to return total from numbersToCalculate & assign that total to firstNumber */
+            numbersToCalculate["firstNumber"] = `${operate(numbersToCalculate["workingOperator"], numbersToCalculate["firstNumber"], numbersToCalculate["secondNumber"])}`;
+            numbersToCalculate["workingOperator"] = operatorInput[buttonID];
+            numbersToCalculate["secondNumber"] = "";
         };
     }
     /* if we want to match the online-calculator example, we should only show the last number input before an operator (don't display the operator) */
     resultsScreen.textContent = `${numbersToCalculate["firstNumber"]} ${numbersToCalculate["workingOperator"]} ${numbersToCalculate["secondNumber"]}`
 
-    // if (operatorInput.hasOwnProperty(button.target.id)) {
+    console.log("At the end:", numbersToCalculate["firstNumber"]);
+    console.log(numbersToCalculate["workingOperator"]);
+    console.log(numbersToCalculate["secondNumber"]);
 
-    // if (button.target.id === "function-clear") {
-    //     resultsScreen.textContent = "";
+    lastButtonPressed = buttonID;
+
     // } else if (button.target.id === "operator-equals") {
     //     resultsScreen.textContent = "" /* return result of operator */
     // } else {
@@ -65,19 +90,19 @@ function displayButton(button) {
 }
 
 function add(num1, num2) {
-    return num1 + num2;
+    return Number(num1) + Number(num2);
 }
 
 function subtract(num1, num2) {
-    return num1 - num2;
+    return Number(num1) - Number(num2);
 }
 
 function multiply(num1, num2) {
-    return num1 * num2;
+    return Number(num1) * Number(num2);
 }
 
 function divide(num1, num2) {
-    return num1 / num2;
+    return Number(num1) / Number(num2);
 }
 
 function operate(operator, num1, num2) {
@@ -87,6 +112,8 @@ function operate(operator, num1, num2) {
         "*": multiply,
         "/": divide,
     }
-
+    // need if statement to check for empty operator / num1 / num2
+    // num1 / num2 get turned into 0 on the Number() calls above
+    // cannot use default arguments on empty strings - only works on undefined or null
     return operators[operator](num1, num2);
 }
