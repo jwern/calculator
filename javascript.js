4db96e68485e6bdddbb730c44631c9dc6c4c6738
Jillian Werner <jillianlwerner@gmail.com>
2020-05-19 14:05:19 -0400

    Fix typeError if no operator is present

--- javascript.js
+++ javascript.js
@@ -12,7 +12,7 @@ let numbersToCalculate = {
 
 let lastButtonPressed = ""; // Updated at the very end of displayButton function so you can refer to it anytime before then
 
-let lastOperation = {
+let lastOperation = { // Used for equations with multiple equals in a row, or no secondNumber given
     lastNumber: "",
     lastOperator: "",
 }
@@ -49,8 +49,9 @@ function displayButton(button) {
         clearNumbersToCalculate();
     }
 
+    /* if button pressed is Backspace, remove the last character in the active number */
     if (buttonID === "function-backspace") {
-        if (lastButtonPressed === "operator-equals") {
+        if (lastButtonPressed === "operator-equals") { // if the last button pressed was "=", we don't want to backspace on that result
             return;
         } else {
             numbersToCalculate[activeNumber()] = numbersToCalculate[activeNumber()].slice(0, -1);
@@ -59,18 +60,21 @@ function displayButton(button) {
     
     /* if button pressed is equals sign (=) */
     if (buttonID === "operator-equals") {
-        if (lastButtonPressed === "operator-equals") {
+        if (lastButtonPressed === "operator-equals") { // e.g. someone presses "2 + 2 = = = " we want to show "4, 6, 8"
             numbersToCalculate["firstNumber"] = `${operate(lastOperation["lastOperator"], numbersToCalculate["firstNumber"], lastOperation["lastNumber"])}`;
         } else {
-            if (numbersToCalculate["secondNumber"] === "") {
+            if (numbersToCalculate["secondNumber"] === emptyString()) { // store lastNumber in case the user hits equals again
                 lastOperation["lastNumber"] = numbersToCalculate["firstNumber"];
             } else {
                 lastOperation["lastNumber"] = numbersToCalculate["secondNumber"];
             };
+            /* complete the equation and assign the result to firstNumber; 
+            the operator to lastOperator (in case the uer hits equals again); 
+            then clear workingOperator and secondNumber. */
             numbersToCalculate["firstNumber"] = `${operate(numbersToCalculate["workingOperator"], numbersToCalculate["firstNumber"], numbersToCalculate["secondNumber"])}`;
             lastOperation["lastOperator"] = numbersToCalculate["workingOperator"];
-            numbersToCalculate["workingOperator"] = "";
-            numbersToCalculate["secondNumber"] = "";
+            numbersToCalculate["workingOperator"] = emptyString();
+            numbersToCalculate["secondNumber"] = emptyString();
         };
     };
 
@@ -79,28 +83,27 @@ function displayButton(button) {
         if (buttonID === "digit-negative") {
             numbersToCalculate[activeNumber()] = addOrRemoveNegative(numbersToCalculate[activeNumber()]);
         } else {
-            if (lastButtonPressed === "operator-equals") { /* if the last button pressed was "=", we're editing from scratch again, not appending digits */
+            if (lastButtonPressed === "operator-equals") { // if the last button pressed was "=", we're editing from scratch again, not appending digits
                 numbersToCalculate[activeNumber()] = digitInput[buttonID];
             } else if (buttonID === "digit-decimal" && numbersToCalculate[activeNumber()].includes(".")) {
                 return; // if the activeNumber already has a decimal in it and decimal is hit again, don't add another decimal - exit function
             } else {
                 numbersToCalculate[activeNumber()] = adjustZeroes(numbersToCalculate[activeNumber()] += digitInput[buttonID]);
-            };
-            
+            }; 
         };
     };
 
     /* if the button pressed is an operator (+, -, *, /) */
     if (operatorInput.hasOwnProperty(buttonID)) {
         if (firstNumberEditable()) {
-            numbersToCalculate["workingOperator"] = operatorInput[buttonID]; /* if there is no operator, assign as operator */
+            numbersToCalculate["workingOperator"] = operatorInput[buttonID]; // if there is no operator, assign as operator
         } else {
             if (operatorInput.hasOwnProperty(lastButtonPressed)) {
-                numbersToCalculate["workingOperator"] = operatorInput[buttonID]; /* if there is an operator that was just added, overwrite it.  Example: 2 + - 1 = 1, not 3 */
-            } else { /* if there is an operator, complete the calculation and assign result to firstNumber, then assign new operator */
+                numbersToCalculate["workingOperator"] = operatorInput[buttonID]; // if there is an operator that was just added, overwrite it.  Example: 2 + - 1 = 1, not 3
+            } else { // if there is an operator and last button was not an operator, complete the calculation and assign result to firstNumber, then assign new operator.
                 numbersToCalculate["firstNumber"] = `${operate(numbersToCalculate["workingOperator"], numbersToCalculate["firstNumber"], numbersToCalculate["secondNumber"])}`;
                 numbersToCalculate["workingOperator"] = operatorInput[buttonID];
-                numbersToCalculate["secondNumber"] = "";
+                numbersToCalculate["secondNumber"] = emptyString();
             };
         };
     }
@@ -110,7 +113,7 @@ function displayButton(button) {
     resultsScreen.textContent = numberToDisplay;
 
     /* remove NaN values before the next calculation */
-    checkForErrors;
+    checkForErrors();
     
     lastButtonPressed = buttonID;
 }
@@ -122,7 +125,7 @@ function checkForErrors() {
 }
 
 function firstNumberEditable() { // Checks if operator is empty, which tells us if we're editing the firstNumber (true) or secondNumber (false)
-    return numbersToCalculate["workingOperator"] === "";
+    return numbersToCalculate["workingOperator"] === emptyString();
 }
 
 function activeNumber() { // Determines which number in numbersToCalculate we're currently editing
@@ -151,10 +154,6 @@ function addZeroes(num) { // Prevent NaN errors if input is only a negative sign
 }
 
 function adjustZeroes(number) {
-    // while (number[0] === "0" && number[1] === "0") { // as long as the first and second digits are zero
-    //     number = number.slice(1); // remove the first 0
-    // };
-
     if (number[0] === "0" && number[1] !== "." && number.length > 1) { // prevents something like "0200", allows "0.0200"
         number = number.slice(1);
     }; 
@@ -166,9 +165,13 @@ function adjustZeroes(number) {
     return addZeroes(number);
 }
 
+function emptyString() {
+    return "";
+}
+
 function clearNumbersToCalculate() {
     for (key in numbersToCalculate) {
-        numbersToCalculate[key] = "";
+        numbersToCalculate[key] = emptyString();
     };
 }
 
@@ -198,6 +201,10 @@ function operate(operator, num1, num2) {
         "-": subtract,
         "*": multiply,
         "/": divide,
+    };
+
+    if (operator === emptyString()) {
+        return Number(num2 || num1);
     };
 
     return operators[operator](num1, num2);
